cb92e1955d58ebf89556a6c9e55c08e38f7a721f
Mitroshin <mitroshin.develop@gmail.com>
2017-01-05 22:42:36 +0300

    Реализован слушатель событий для поля ввода заголовка

--- app/src/main/java/com/developgmail/mitroshin/todo/controllers/TaskFragment.java
+++ app/src/main/java/com/developgmail/mitroshin/todo/controllers/TaskFragment.java
@@ -3,9 +3,12 @@ package com.developgmail.mitroshin.todo.controllers;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.developgmail.mitroshin.todo.R;
 import com.developgmail.mitroshin.todo.model.Task;
@@ -20,6 +23,9 @@ public class TaskFragment extends Fragment {
     /*Ссылка на объект модели*/
     private Task mTask;
 
+    /*Ссылки на виджеты представления*/
+    private EditText mEditTextTaskTitle;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +38,44 @@ public class TaskFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
         /*Заполнение макета.
         Передается идентификатор ресурса макета,
         родитель представления,
         нужно ли включать заполненное представление в родителя*/
         View view = inflater.inflate(R.layout.fragment_task_details, container, false);
+
+        findAllViewById(view);
+        addAllListeners();
+
         return view;
+    }
+
+    /*Метод обавляет слушатели события для виджетов*/
+    private void addAllListeners() {
+        /*Слушатель для поля ввода заголовка*/
+        mEditTextTaskTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mTask.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    /*Метод связывает объекты контроллера с объектами представления*/
+    private void findAllViewById(View view) {
+        mEditTextTaskTitle = (EditText) view.findViewById(R.id.f_task_details_edit_text_task_title);
     }
 }
