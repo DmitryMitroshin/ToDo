90b4cd327850fa5e29a8d9525efe46eac916e7a5
Mitroshin <mitroshin.develop@gmail.com>
2017-01-10 17:21:09 +0300

    Добавлен адаптер списка для его отображения во ViewPager

--- app/src/main/java/com/developgmail/mitroshin/todo/host/TaskPagerActivity.java
+++ app/src/main/java/com/developgmail/mitroshin/todo/host/TaskPagerActivity.java
@@ -2,17 +2,51 @@ package com.developgmail.mitroshin.todo.host;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.developgmail.mitroshin.todo.R;
+import com.developgmail.mitroshin.todo.controllers.TaskFragment;
+import com.developgmail.mitroshin.todo.model.Task;
+import com.developgmail.mitroshin.todo.util.TaskLab;
+
+import java.util.List;
 
 /*Хост для фрагмента TaskFragment.java*/
 
 public class TaskPagerActivity extends FragmentActivity {
 
+    /*Ссылка на виджет ViewPager*/
+    private ViewPager mViewPager;
+    /*Ссылка на список, для отображения*/
+    private List<Task> mListTask;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.a_view_pager_task);
+        mListTask = TaskLab.getTaskLab(this).getListTask();
+
+        /*Адаптер отвечает за взаимодействие данных с ViewPager.
+        Для этого ему необходима возможность возвращать экземпляры фрагментов в getItem()
+        с помощью объекта класса FragmentManager.*/
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Task task = mListTask.get(position);
+                return TaskFragment.newInstance(task.getUUID());
+            }
+
+            @Override
+            public int getCount() {
+                return mListTask.size();
+            }
+        });
     }
 }
